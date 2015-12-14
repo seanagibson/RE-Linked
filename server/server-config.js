b85bd6194c80442d33f57d8f591827fec1b85795
seanagibson <seanandgibson@gmail.com>
2015-12-14 12:26:10 -0800

    Added body-parser module

--- server/server-config.js
+++ server/server-config.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 // var requestHandler = require('./request-handler.js');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '../../client'));
 
 
